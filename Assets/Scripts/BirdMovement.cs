2899ca99238f78869de7f374995eb7fdb28d68d8
lucas gradilone <lucasgradilone@gmail.com>
2024-05-15 20:14:00 -0300

    Sons de voo criado

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -7,17 +7,28 @@ public class BirdMovement : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Animator _animator;
 
+    public AudioClip clickSound;
+    private AudioSource audioSource;
+
     private void Start()
     {
+        audioSource = gameObject.GetComponent<AudioSource>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.clip = clickSound;
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
+
             _rigidbody.velocity = Vector2.up * speed;
             _animator.SetTrigger("Jump");
+            audioSource.Play();
         }
     }
 }
